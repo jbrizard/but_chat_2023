eacb74b05817afff63f5b4fa5a5f0882f2d0042a
Thrywich <aurel.thry@gmail.com>
2023-09-06 13:11:57 +0200

    Ajout feedback.js dans server/modules

--- server/modules/feedback.js
+++ server/modules/feedback.js
@@ -3,3 +3,16 @@
  * Description : Ce module affiche qui est en train d'écrire
  * Auteur(s) : Alexandre BELLET, Aurélien THIERRY
  */
+
+// Définit les méthodes "publiques" (utilisation à l'extérieur du module)
+module.exports =  {
+	writing: writing // permet d'appeler cette méthode dans server.js -> feedback.writeOn(...)
+}
+
+/**
+ * Lorsqu'on appelle Feedback ...
+ */
+function writing(io, message)
+{
+	
+}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var fs = require('fs');			// Accès au système de fichier
 
 // Chargement des modules perso
 var daffy = require('./modules/daffy.js');
+var feedback  = require('./modules/feedback.js');
 
 // Initialisation du serveur HTTP
 var app = express();
