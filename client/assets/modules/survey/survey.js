9039c6c009d2611652271db1f22bd7fb9351aad9
lorenzo-bravard <103388200+lorenzo-bravard@users.noreply.github.com>
2023-09-13 16:13:22 +0200

    Timer final

--- client/assets/modules/survey/survey.js
+++ client/assets/modules/survey/survey.js
@@ -1,12 +1,15 @@
 socket.on('new_survey', receiveSurvey);
+socket.on('new_time', receiveTime);
 socket.on('new_count', receiveCount);
+socket.on('new_winner', receiveWinner);
 
 $('#create-survey').click(sendSurvey);
 
+/**
+ * Envoie d'un sondage au serveur
+ */
 function sendSurvey()
 {
-	
-
 	// Récupère le message, puis vide le champ texte
 	var surveyInput = $('#survey-input');
 	var choice1Input = $('#choice1');
@@ -18,9 +21,6 @@ function sendSurvey()
 	surveyInput.val('');
 	choice1Input.val('');
 	choice2Input.val('');
-
-
-	
 	
 	// On n'envoie pas un message vide
 	if (survey == '' || choice1 == '' || choice2 == '' )
@@ -28,10 +28,29 @@ function sendSurvey()
 	
 	// Envoi le message au serveur pour broadcast
 	socket.emit('survey', survey);
-
 	}
 
+/**
+ * Affiche le temps restant reçu par le serveur
+ */
+function receiveTime(data)
+{
+	$('#surveys').append(
+		'<span id="timer"></span>'
+	)
+	
+	const timerElement = document.getElementById("timer");
+	timerElement.innerText = `${data.minutes}:${data.secondes}`;
+	
+	if (data.secondes == 0)
+	{
+		timerElement.innerText = '';
+	}
+}
 
+/**
+ * Affichage du sondage reçu par le serveur
+ */
 function receiveSurvey(data)
 {	
 	$('.survey').replaceWith('');
@@ -49,6 +68,7 @@ function receiveSurvey(data)
 			+'</div>'
 	     + '</div>'
 	)
+
 	var button1 = $('#surveyButton1');
 	var button2 = $('#surveyButton2');
 	
@@ -56,7 +76,9 @@ function receiveSurvey(data)
 	button2.click(surveyClickCount);
 }
 
-
+/**
+ * Desactive les buttons au click et initialise le vote
+ */
 function surveyClickCount()
 {
 	var n;
@@ -64,20 +86,16 @@ function surveyClickCount()
 	var counter = {addVote, n}
 	var countId = this.id
 	socket.emit('count', counter, countId);
-	console.log(this.id);
-	$('.surveyButtons').prop('disabled', true);
-
-	
+	$('.surveyButtons').prop('disabled', true);	
 }
 
-
-
+/**
+ * Affiche le nombre de vote reçu par le serveur
+ */
 function receiveCount(data)
 {
-	console.log(data.id);
 	if (data.compterId == "surveyButton1")
 	{
-		console.log(data.numVote1);
 		$('#count1').replaceWith('<p id="count1">'+data.numVote1+'</p>');
 	}
 	else
@@ -87,5 +105,40 @@ function receiveCount(data)
 	
 }
 	
-
-	
+/**
+ * Affiche le résultat gagnant du sondage reçu par le serveur
+ */
+function receiveWinner(data)
+{
+	$('.survey').replaceWith('');
+	if (data.winner < 0)
+	{
+		$('#chat #messages').append(
+			'<div class="message">'
+				+ '<span class="winner">Résultat</span> ' 
+				+ data.choice1
+			 + '</div>'
+		)
+		.scrollTop(function(){ return this.scrollHeight });  // scrolle en bas du conteneur
+	}
+	if (data.winner > 0)
+	{
+		$('#chat #messages').append(
+			'<div class="message">'
+				+ '<span class="winner">Résultat</span> ' 
+				+ data.choice2
+			 + '</div>'
+		)
+		.scrollTop(function(){ return this.scrollHeight });  // scrolle en bas du conteneur
+	}
+	if (data.winner == 0)
+	{
+		$('#chat #messages').append(
+			'<div class="message">'
+				+ '<span class="winner">Résultat</span> ' 
+				+ data.choice2 + 'et' + data.choice2
+			 + '</div>'
+		)
+		.scrollTop(function(){ return this.scrollHeight });  // scrolle en bas du conteneur
+	}	
+}
--- server/modules/survey.js
+++ server/modules/survey.js
@@ -12,6 +12,9 @@ module.exports =  {
 var numVote1 = 0;
 var numVote2 = 0;
 
+var choice1;
+var choice2;
+
 function handleNewConnection(socket, io)
 {
     socket.on('survey', function(survey)
@@ -20,36 +23,70 @@ function handleNewConnection(socket, io)
         // survey = ent.encode(survey.surveyValue);
         numVote1 = 0;
         numVote2 = 0;
-            
+        choice1 = survey['choice1'];
+        choice2 = survey['choice2'];
         // Transmet le message à tous les utilisateurs (broadcast)
         io.sockets.emit('new_survey', 
             {
                 name:socket.name, 
                 surveyName:survey['surveyValue'], 
-                choice1:survey['choice1'], 
-                choice2:survey['choice2']
+                choice1:choice1, 
+                choice2:choice2
             }
         );
-        
+        timer();
     });	
 
-    socket.on('count', function(counter, countId)
+    socket.on('count', function(counter, countId, temps)
     {
         var compterId = countId;
+        var compteurTemps = temps;
+        
         if (compterId == "surveyButton1")
         {
             numVote1 += 1;
-            console.log(numVote1);
-            
+
             io.sockets.emit('new_count', {numVote1:numVote1, n:counter['n'], compterId:compterId  });
-        }
+        }   
         else
         {
             numVote2 += counter['addVote'];
+
             io.sockets.emit('new_count', {numVote2:numVote2, n:counter['n'], compterId:compterId  });
         }
-        
+
     });	
+
+
+function timer()
+{
+	const departMinutes = 0.1;
+	let temps = departMinutes * 60;
+	var interval = setInterval(() => {
+	let minutes = parseInt(temps / 60, 10);
+	let secondes = parseInt(temps % 60, 10);
+	
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	secondes = secondes < 10 ? "0" + secondes : secondes;
+	
+	if (temps > 0)
+	{
+	    temps = temps - 1;
+        io.sockets.emit('new_time', {temps:temps, minutes:minutes, secondes:secondes});
+	} else
+	{
+		temps = 0;
+        io.sockets.emit('new_time', {temps:temps, minutes:minutes, secondes:secondes});
+        var winner = numVote1 - numVote2;
+    
+        io.sockets.emit('new_winner', {winner:winner, name:socket.name, choice1:choice1, choice2:choice2 });
+
+		clearInterval(interval);
+	}
+	}, 1000)
+	
+}
+
 }
  
 
