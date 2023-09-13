a32f486760437805cb718d9f35b2e064d970b888
XELAmp4 <alexandre.bellet0321@gmail.com>
2023-09-13 16:44:04 +0200

    Ajout des photos de profil dans l'état de connection

--- client/assets/modules/connected/connectedClient.js
+++ client/assets/modules/connected/connectedClient.js
@@ -5,13 +5,20 @@ function receiveUserConnected(data) {
     $('#connected-content *').remove();
 
     data.forEach(user => {
-        $('#connected-content').append('<li id="' + user.id + '"><img/><div><p>' + user.name + '</p></div></li>');
+        $('#connected-content').append('<li id="' + user.id + '"><img src="' + user.avatar + '"/><div><p>' + user.name + '</p></div></li>');
     });
 }
 
 socket.on('disconnected', userDisconnected);
 
 function userDisconnected(id) {
-    //console.log(id);
     $('#connected-content li#' + id).remove();
+}
+
+
+socket.on('new_avatar_for_all',changeAvatar);
+
+function changeAvatar(data) 
+{
+    $('#connected-content li#' + data.socketId +'>img').attr('src',data.avatar);
 }
