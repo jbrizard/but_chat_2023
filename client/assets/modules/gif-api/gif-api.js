714a623c65edeefe95f0d4d04b44db598e9942c6
tak000 <just.acmora@free.fr>
2023-09-13 16:47:03 +0200

    improved: gif pannel

--- client/assets/modules/gif-api/gif-api.js
+++ client/assets/modules/gif-api/gif-api.js
@@ -1,18 +1,20 @@
 const input = document.querySelector("#gif-panel input");
 const button = document.querySelector("#gif-panel button");
+const gifPanel = document.querySelector("#gif-panel");
+
 let i = 0;
 
 button.addEventListener("click",() => {
   searchGif(input.value);
 })
 
+document.querySelector('#send-gif').addEventListener("click", () => {
+  gifPanel.style.display = "flex";
+})
 
 // listen for file sharings
 socket.on('search_gif_result', searchGifResult);
-
 socket.on('receive_gif', receivingGif);
-
-
 
 
 function searchGif(search_term){
@@ -22,16 +24,24 @@ function searchGif(search_term){
 
 function searchGifResult(data){
   const resultsDom = document.querySelector("#gif-panel #results");
-  resultsDom.innerHTML = "";
-  for(const element of data.results){
+  resultsDom.querySelectorAll("div").forEach(el => el.innerHTML = "")
+  data.results.forEach((element, index) => {
     let img = document.createElement("img");
     img.src = element.media[0].nanogif.url;
     img.onclick = () => {sendGif(element)};
-    resultsDom.appendChild(img);
-  }
+
+    if(index > (data.results.length / 2)){
+      document.querySelector("#gif-panel #results>div:last-of-type").appendChild(img);
+    }else{
+      document.querySelector("#gif-panel #results>div:first-of-type").appendChild(img);
+    }
+
+
+  });
 }
 
 function sendGif(data){
+  gifPanel.style.display = "none";
   socket.emit('send_gif', data);
 }
 
